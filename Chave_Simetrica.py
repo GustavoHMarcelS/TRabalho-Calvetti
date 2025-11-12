ff691729fa4776a71fe9bdb4e42377e98601963f
GustavoHMarcelS <gustavohmarcel@gmail.com>
2025-11-11 21:27:50 -0300

    Add files via upload

---
+++ Chave_Simetrica.py
@@ -0,0 +1,27 @@
+from cryptography.fernet import Fernet
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import hashlib
+
+#"CHAVE SIMETRICA USA A MESMA CHAVE"
+
+print("=== CRIPTOGRAFIA SIMÉTRICA ===")
+
+#"CHAVE SENDO GERADA"
+chave_simetrica = Fernet.generate_key()
+fernet = Fernet(chave_simetrica)
+
+#Texto original
+mensagem = "Segurança é importante!"
+
+# Criptografar
+mensagem_criptografada = fernet.encrypt(mensagem.encode())
+
+# Descriptografar
+mensagem_original = fernet.decrypt(mensagem_criptografada).decode()
+
+print("Chave:", chave_simetrica)
+print("Mensagem criptografada:", mensagem_criptografada)
+print("Mensagem original:", mensagem_original)
+
+print("\n-------------------------------------------\n")
